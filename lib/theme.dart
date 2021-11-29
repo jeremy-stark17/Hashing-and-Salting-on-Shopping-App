c6254ba2eecc2f7fcff7163c1715245ae75dab06
jeremy-stark17 <jeremiahpothula37@gmail.com>
2021-11-28 19:18:50 -0700

    updated firebase

---
+++ lib/theme.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: duplicate_ignore, avoid_web_libraries_in_flutter
+
+// ignore: unused_import
+
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    fontFamily: "Muli",
+    appBarTheme: appBarTheme(),
+    textTheme: textTheme(),
+    inputDecorationTheme: inputDecorationTheme(),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
+
+InputDecorationTheme inputDecorationTheme() {
+  OutlineInputBorder outlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(28),
+    borderSide: BorderSide(color: kTextColor),
+    gapPadding: 10,
+  );
+  return InputDecorationTheme(
+    // If  you are using latest version of flutter then lable text and hint text shown like this
+    // if you r using flutter less then 1.20.* then maybe this is not working properly
+    // if we are define our floatingLabelBehavior in our theme then it's not applayed
+    floatingLabelBehavior: FloatingLabelBehavior.always,
+    contentPadding: EdgeInsets.symmetric(horizontal: 42, vertical: 20),
+    enabledBorder: outlineInputBorder,
+    focusedBorder: outlineInputBorder,
+    border: outlineInputBorder,
+  );
+}
+
+TextTheme textTheme() {
+  return TextTheme(
+    bodyText1: TextStyle(color: kTextColor),
+    bodyText2: TextStyle(color: kTextColor),
+  );
+}
+
+AppBarTheme appBarTheme() {
+  // ignore: dead_code, dead_code, dead_code
+  var appBarTheme3 = AppBarTheme(
+    color: Colors.white,
+    elevation: 0,
+    iconTheme: IconThemeData(color: Colors.black),
+  );
+  var appBarTheme32 = appBarTheme3;
+  var appBarTheme = appBarTheme32;
+  var appBarTheme2 = appBarTheme;
+  var appBarTheme22 = appBarTheme2;
+  return appBarTheme22;
+}
